0f35225f4810d6eb9d04b000edda9dab2da2f288
Moses Achi <mosesachi.me@gmail.com>
2025-10-28 05:11:31 +0100

    Move all project files to repo root

--- ml-model-serving/app/models/init.py
+++
@@ -1,8 +0,0 @@
-# Models package
-from app.models.schemas import PredictionRequest, PredictionResponse, ModelInfo
-from app.models.database import PredictionRecord, ModelMetadata, ErrorLog
-
-__all__ = [
-    "PredictionRequest", "PredictionResponse", "ModelInfo",
-    "PredictionRecord", "ModelMetadata", "ErrorLog"
-]
